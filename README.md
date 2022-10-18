3d9ef2a63da7bb7ca48f0eaef1312ecb7fd1f9e2
danielgregoire34 <danielgregoire34@gmail.com>
2022-10-17 22:43:14 -0400

    My first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Password Generator Starter Code
+
+## Script.JS
+In ```generatepassword()``` I ended up creating most of the code within that function
+
+I created characters that had a list of all the alphebet and special characters in ```char```
+
+I used ```passwordLength``` and ```password``` as the other variables within the ```for``` loop equation. 
+
+In the ```for```loop equation the ```var randomNumber``` and the ```password``` make up the equation to get the random password generator using the different characters.
---
+++ script.js
@@ -0,0 +1,35 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+writePassword();
+
+function generatePassword(){
+
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  var passwordLength = 7;
+  var password = "";
+
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
+  console.log(chars);
+    return password;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
